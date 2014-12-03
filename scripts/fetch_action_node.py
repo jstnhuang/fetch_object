73ddafc0de589de98225e7e30c732e3e78a71b2a
Justin Huang <jstn@cs.washington.edu>
2014-12-02 19:29:19 -0800

    Lots of changes, implemented up to navigating to destination.

--- scripts/fetch_action_node.py
+++ scripts/fetch_action_node.py
@@ -6,26 +6,22 @@ from geometry_msgs.msg import Pose
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Quaternion
 from move_base_msgs.msg import MoveBaseAction
+from object_manipulation_msgs.msg import PickupAction
 from pr2_common_action_msgs.msg import TuckArmsAction
 from pr2_controllers_msgs.msg import PointHeadAction
+from pr2_controllers_msgs.msg import Pr2GripperCommandAction
 from pr2_controllers_msgs.msg import SingleJointPositionAction
-from tabletop_object_detector.srv import TabletopSegmentation
+from tabletop_collision_map_processing.srv import TabletopCollisionMapProcessing
+from tabletop_object_detector.srv import TabletopDetection
 import actionlib
 import rospy
 import smach
 import states
 
-#from states import AdjustPoseForTabletopManipulation
-#from states import DetectTabletopObjects
-#from states import ProcessCollisionMap
-#from states import PickupObject
-#from states import PrepareNavigationToDestination
-#from states import NavigateToDestination
-
 def main():
   rospy.init_node('fetch_object_action')
 
-  sm = smach.StateMachine(outcomes=['SUCCESS', 'FAILURE'])
+  sm = smach.StateMachine(outcomes=['SUCCESS', 'FAILED_BUT_RESET', 'FAILURE'])
   
   torso_client = actionlib.SimpleActionClient(
     'torso_controller/position_joint_action',
@@ -43,25 +39,37 @@ def main():
     'move_base',
     MoveBaseAction
   )
-  rospy.wait_for_service('tabletop_segmentation')
-  segment_tabletop = rospy.ServiceProxy(
-    'tabletop_segmentation',
-    TabletopSegmentation
+  rospy.wait_for_service('/object_detection')
+  detect_objects = rospy.ServiceProxy(
+    '/object_detection',
+    TabletopDetection
   )
-  base_publisher = rospy.Publisher('/base_controller/command', Twist)
+  rospy.wait_for_service('/tabletop_collision_map_processing/tabletop_collision_map_processing')
+  process_collision_map = rospy.ServiceProxy(
+    '/tabletop_collision_map_processing/tabletop_collision_map_processing',
+    TabletopCollisionMapProcessing
+  )
+  pickup_client = actionlib.SimpleActionClient(
+    '/object_manipulator/object_manipulator_pickup',
+    PickupAction
+  )
+  r_gripper_client = actionlib.SimpleActionClient(
+    'r_gripper_controller/gripper_action',
+    Pr2GripperCommandAction
+  )
 
   # Hard-coded poses for testing.
+  STARTING_POSE = Pose(
+    position=Point(-1, 0.5, 0),
+    orientation=Quaternion(0, 0, 0, 1)
+  )
   POSE_A = Pose(
     position=Point(3.86, -0.293, 0),
     orientation=Quaternion(0, 0, -0.045, 0.999)
   )
-  POSE_TEST = Pose(
-    position=Point(0, 0, 0),
-    orientation=Quaternion(0, 0, -0.0614, 0.998)
-  )
   POSE_B = Pose(
-    position=Point(-0.02, -3.73, 0),
-    orientation=Quaternion(0, 0, 0.997, 0.756)
+    position=Point(0.15, -3.57, 0),
+    orientation=Quaternion(0, 0, 0.997, 0.028)
   )
 
   with sm:
@@ -70,29 +78,59 @@ def main():
       states.PrepareNavigationToTable(torso_client, tuck_arms_client),
       transitions={
         'prepare_nav_success': 'NAVIGATE_TO_TABLE',
-        'prepare_nav_failure': 'FAILURE' 
+        'prepare_nav_failure': 'RESET' 
       }
     )
     smach.StateMachine.add(
       'NAVIGATE_TO_TABLE',
       states.NavigateToTable(
-        head_client,
         nav_client,
-        base_publisher,
-        segment_tabletop,
         POSE_A
       ),
       transitions={
         'nav_to_table_success': 'PREPARE_PICK',
-        'nav_to_table_failure': 'FAILURE'
+        'nav_to_table_failure': 'RESET'
       }
     )
     smach.StateMachine.add(
       'PREPARE_PICK',
-      states.PreparePickObject(torso_client, tuck_arms_client),
+      states.PreparePickObject(head_client, torso_client, tuck_arms_client),
       transitions={
-        'prepare_pick_success': 'SUCCESS',
-        'prepare_pick_failure': 'FAILURE'
+        'prepare_pick_success': 'PICK_OBJECT',
+        'prepare_pick_failure': 'RESET'
+      }
+    )
+    smach.StateMachine.add(
+      'PICK_OBJECT',
+      states.PickObject(detect_objects, process_collision_map, pickup_client),
+      transitions={
+        'pick_success': 'PREPARE_TRANSPORT',
+        'pick_failure': 'RESET'
+      }
+    )
+    smach.StateMachine.add(
+      'PREPARE_TRANSPORT',
+      states.PrepareTransport(tuck_arms_client),
+      transitions={
+        'prepare_transport_success': 'NAVIGATE_TO_DESTINATION',
+        'prepare_transport_failure': 'RESET'
+      }
+    )
+    smach.StateMachine.add(
+      'NAVIGATE_TO_DESTINATION',
+      states.NavigateToDestination(nav_client, POSE_B),
+      transitions={
+        'nav_to_destination_success': 'RESET',
+        'nav_to_destination_failure': 'RESET'
+      }
+    )
+    smach.StateMachine.add(
+      'RESET',
+      states.Reset(nav_client, r_gripper_client, tuck_arms_client,
+        STARTING_POSE),
+      transitions={
+        'reset_success': 'FAILED_BUT_RESET',
+        'reset_failure': 'FAILURE'
       }
     )
   outcome = sm.execute()
--- scripts/states.py
+++ scripts/states.py
@@ -3,11 +3,17 @@ import roslib; roslib.load_manifest('fetch_object')
 
 from actionlib_msgs.msg import GoalStatus
 from geometry_msgs.msg import Point
+from geometry_msgs.msg import Quaternion
 from geometry_msgs.msg import PointStamped
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Vector3Stamped
 from move_base_msgs.msg import MoveBaseGoal
+from pr2_controllers_msgs.msg import Pr2GripperCommandGoal
+from pr2_controllers_msgs.msg import Pr2GripperCommand
+from object_manipulation_msgs.msg import GripperTranslation
+from object_manipulation_msgs.msg import PickupGoal
 from pr2_common_action_msgs.msg import TuckArmsGoal
 from pr2_controllers_msgs.msg import PointHeadGoal
 from pr2_controllers_msgs.msg import SingleJointPositionGoal
@@ -16,6 +22,10 @@ import actionlib
 import rospy
 import smach
 import tf
+
+def check_success_state(client):
+  state = client.get_state()
+  return state == GoalStatus.SUCCEEDED
 
 class PrepareNavigationToTable(smach.State):
   """Prepares the robot to move with nothing in hand.
@@ -28,7 +38,7 @@ class PrepareNavigationToTable(smach.State):
 
     Args:
       torso_client: An action client for the PR2 torso.
-      tuck_arms_client: An action client for the tuck arms action.
+      tuck_arms_client: An action client for tucking the PR2's arms.
     """
     smach.State.__init__(
       self,
@@ -40,63 +50,92 @@ class PrepareNavigationToTable(smach.State):
   def execute(self, userdata):
     rospy.loginfo('Executing PrepareNavigationToTable')
     self._torso_client.wait_for_server()
-    goal = SingleJointPositionGoal(
-      position=0.02, # PR2 can't go below 0.0115 for some reason.
+    torso_goal = SingleJointPositionGoal(
+      # TODO(jstn): We don't actually lower the base so we don't have to wait
+      # around as much when testing.
+      position=0.2, # PR2 can't go below 0.0115 for some reason.
       min_duration=rospy.Duration.from_sec(1),
       max_velocity=1
     )
-    self._torso_client.send_goal(goal)
-    self._torso_client.wait_for_result() # Torso client returns nothing.
+    self._torso_client.send_goal(torso_goal)
+    self._torso_client.wait_for_result()
+    if not check_success_state(self._torso_client):
+      rospy.logerr('Failed to lower torso.')
+      return 'prepare_nav_failure'
 
     self._tuck_arms_client.wait_for_server()
-    goal = TuckArmsGoal(tuck_left=True, tuck_right=True)
-    self._tuck_arms_client.send_goal(goal)
+    tuck_arms_goal = TuckArmsGoal(tuck_left=True, tuck_right=True)
+    self._tuck_arms_client.send_goal(tuck_arms_goal)
     self._tuck_arms_client.wait_for_result()
-    result = self._tuck_arms_client.get_result()
-    if result is None:
+    if not check_success_state(self._tuck_arms_client):
+      rospy.logerr('Failed to tuck arms.')
       return 'prepare_nav_failure'
-    elif result.tuck_left and result.tuck_right:
-      return 'prepare_nav_success'
-    else:
-      return 'prepare_nav_failure'
+    return 'prepare_nav_success'
 
 class NavigateToTable(smach.State):
   """Navigates the robot to a table.
-
-  First, the robot navigates to the given goal pose. Once it's done navigating,
-  it searches for a table, and then adjusts its position to get right next to
-  the table.
   """
 
-  def __init__(self, head_client, nav_client, base_publisher, segment_tabletop,
-      goal_pose):
+  def __init__(self, nav_client, goal_pose):
     """Constructor.
 
     Args:
-      head_client: An action client for moving the PR2's head.
       nav_client: An action client for navigating with the PR2.
-      base_publisher: A Publisher for the base controller.
-      tabletop_segmentation_service: A ServiceProxy for the tabletop segmenter.
       goal_pose: The destination, relative to the map.
     """
     smach.State.__init__(
       self,
       outcomes=['nav_to_table_success', 'nav_to_table_failure']
     )
-    self._head_client = head_client
     self._nav_client = nav_client
-    # TODO(jstn): unused
-    self._base_publisher = base_publisher
-    self._segment_tabletop = segment_tabletop
     self._goal_pose = goal_pose
-    self._tf = tf.TransformerROS()
 
   def execute(self, userdata):
     rospy.loginfo('Executing NavigateToTable')
+    
+    # Move roughly to where the table is.
+    self._nav_client.wait_for_server()
+    move_base_goal = MoveBaseGoal(
+      target_pose=PoseStamped(
+        header=Header(frame_id='/map'),
+        pose=self._goal_pose
+      )
+    )
+    self._nav_client.send_goal(move_base_goal)
+    self._nav_client.wait_for_result()
+    if not check_success_state(self._nav_client):
+      rospy.logerr('Failed to roughly navigate to table.')
+      return 'nav_to_table_failure'
+    return 'nav_to_table_success'
+
+class PreparePickObject(smach.State):
+  """Prepares the robot to pick up an object on a table.
+
+  It raises the base and deploys the arms out in front of it.
+  """
+
+  def __init__(self, head_client, torso_client, tuck_arms_client):
+    """Constructor.
+
+    Args:
+      head_client: An action client for moving the PR2's head.
+      torso_client: An action client for the PR2 torso.
+      tuck_arms_client: An action client for tucking the PR2's arms.
+    """
+    smach.State.__init__(
+      self,
+      outcomes=['prepare_pick_success', 'prepare_pick_failure']
+    )
+    self._head_client = head_client
+    self._torso_client = torso_client
+    self._tuck_arms_client = tuck_arms_client
+
+  def execute(self, userdata):
+    rospy.loginfo('Executing PreparePickObject')
 
     # Look down.
     self._head_client.wait_for_server()
-    goal = PointHeadGoal(
+    point_head_goal = PointHeadGoal(
       target=PointStamped(
         header=Header(frame_id='/base_link'),
         point=Point(1, 0, 0.6)
@@ -106,97 +145,239 @@ class NavigateToTable(smach.State):
       min_duration=rospy.Duration.from_sec(0.5),
       max_velocity=1
     )
-    self._head_client.send_goal(goal)
+    self._head_client.send_goal(point_head_goal)
     self._head_client.wait_for_result()
+    if not check_success_state(self._head_client):
+      rospy.logerr('Failed to look down.')
+      return 'prepare_pick_failure'
 
-    # Move roughly to where the table is.
+    self._torso_client.wait_for_server()
+    # TODO(jstn): raise to a height based on the table's height.
+    torso_goal = SingleJointPositionGoal(
+      position=0.20,
+      min_duration=rospy.Duration.from_sec(1),
+      max_velocity=1
+    )
+    self._torso_client.send_goal(torso_goal)
+    self._torso_client.wait_for_result()
+    if not check_success_state(self._torso_client):
+      rospy.logerr('Failed to raise torso.')
+      return 'prepare_pick_failure'
+
+    # TODO(jstn): Move to PickObject if it looks like the arms get in the way
+    # too much.
+    self._tuck_arms_client.wait_for_server()
+    tuck_arms_goal = TuckArmsGoal(tuck_left=True, tuck_right=False)
+    self._tuck_arms_client.send_goal(tuck_arms_goal)
+    self._tuck_arms_client.wait_for_result()
+    if not check_success_state(self._tuck_arms_client):
+      rospy.logerr('Failed to untuck right arm.')
+      return 'prepare_pick_failure'
+    return 'prepare_pick_success'
+
+class PickObject(smach.State):
+  """Picks up an object on the table.
+  """
+
+  def __init__(self, detect_objects, process_collision_map, pickup_client):
+    """Constructor.
+    """
+    smach.State.__init__(
+      self,
+      outcomes=['pick_success', 'pick_failure']
+    )
+    self._detect_objects = detect_objects
+    self._process_collision_map = process_collision_map
+    self._pickup_client = pickup_client
+
+  def execute(self, userdata):
+    rospy.loginfo('Executing PickObject')
+
+    detection_response = None
+    try:
+      # We don't use the object database. These names are a bit confusing but
+      # the values are correct.
+      detection_response = self._detect_objects(
+        return_models=True,
+        return_clusters=False
+      )
+    except rospy.ServiceException:
+      rospy.logerr('Failed to detect objects.')
+      return 'pick_failure'
+
+    try:
+      collision_response = self._process_collision_map(
+        detection_result=detection_response.detection,
+        reset_collision_models=True,
+        reset_attached_models=True,
+        desired_frame='base_link'
+      )
+    except rospy.ServiceException:
+      rospy.logerr('Failed to process collision map.')
+      return 'pick_failure'
+
+    if (
+        len(collision_response.graspable_objects) == 0
+        or len(collision_response.collision_object_names) == 0
+    ):
+      rospy.logerr('No objects found.')
+      return 'pick_failure'
+
+    self._pickup_client.wait_for_server()
+    pickup_goal = PickupGoal(
+      target=collision_response.graspable_objects[0],
+      collision_object_name=collision_response.collision_object_names[0],
+      collision_support_surface_name=collision_response.collision_support_surface_name,
+      arm_name='right_arm',
+      lift=GripperTranslation(
+        direction=Vector3Stamped(
+          header=Header(frame_id='base_link'),
+          vector=Vector3(x=0, y=0, z=1)
+        ),
+        desired_distance=0.1,
+        min_distance=0.05
+      ),
+      use_reactive_lift=False,
+      use_reactive_execution=False
+    )
+    self._pickup_client.send_goal(pickup_goal)
+    self._pickup_client.wait_for_result()
+    if not check_success_state(self._pickup_client):
+      rospy.logerr('Failed to pick up object.')
+      return 'pick_failure'
+    return 'pick_success'
+
+class PrepareTransport(smach.State):
+  """Prepares the robot to move a held object.
+  """
+
+  def __init__(self, tuck_arms_client):
+    """Constructor.
+
+    Args:
+      tuck_arms_client: An action client for tucking the PR2's arms.
+    """
+    smach.State.__init__(
+      self,
+      outcomes=['prepare_transport_success', 'prepare_transport_failure']
+    )
+    self._tuck_arms_client = tuck_arms_client
+
+  def execute(self, userdata):
+    rospy.loginfo('Executing PrepareTransport')
+
+    self._tuck_arms_client.wait_for_server()
+    # TODO(jstn): We should try to hold the object level and out to the side
+    # instead of tucking the arms.
+    tuck_arms_goal = TuckArmsGoal(tuck_left=True, tuck_right=True)
+    self._tuck_arms_client.send_goal(tuck_arms_goal)
+    self._tuck_arms_client.wait_for_result()
+    if not check_success_state(self._tuck_arms_client):
+      rospy.logerr('Failed to tuck arms for transport.')
+      return 'prepare_transport_failure'
+    return 'prepare_transport_success'
+
+class NavigateToDestination(smach.State):
+  """Navigates the robot to the destination table.
+  """
+
+  def __init__(self, nav_client, goal_pose):
+    """Constructor.
+
+    Args:
+      nav_client: An action client for navigating with the PR2.
+      goal_pose: The destination, relative to the map.
+    """
+    smach.State.__init__(
+      self,
+      outcomes=['nav_to_destination_success', 'nav_to_destination_failure']
+    )
+    self._nav_client = nav_client
+    self._goal_pose = goal_pose
+
+  def execute(self, userdata):
+    rospy.loginfo('Executing NavigateToDestination')
+
+    # Move roughly to where the destination table is.
     self._nav_client.wait_for_server()
-    goal = MoveBaseGoal(
+    move_base_goal = MoveBaseGoal(
       target_pose=PoseStamped(
         header=Header(frame_id='/map'),
         pose=self._goal_pose
       )
     )
-    self._nav_client.send_goal(goal)
+    self._nav_client.send_goal(move_base_goal)
     self._nav_client.wait_for_result()
-    state = self._nav_client.get_state()
-    if state != GoalStatus.SUCCEEDED:
-      rospy.logerr('Failed to roughly navigate to table.')
-      return 'nav_to_table_failure'
-    return 'nav_to_table_success'
+    if not check_success_state(self._nav_client):
+      rospy.logerr('Failed to roughly navigate to destination table.')
+      return 'nav_to_destination_failure'
+    return 'nav_to_destination_success'
 
-    # Find the table and drive up to it.
-    #try:
-    #  response = self._segment_tabletop()
-    #  rospy.loginfo('Table found: {}'.format(response.table))
-    #  if len(response.table.convex_hull.triangles) == 0:
-    #    rospy.logerr('Couldn\'t find table while navigating.')
-    #    return 'nav_to_table_failure'
-    #  goal = MoveBaseGoal(
-    #    target_pose=PoseStamped(
-    #      header=Header(frame_id=response.table.pose.header.frame_id),
-    #      pose=Pose(
-    #        position=Point(
-    #          response.table.x_min - 0.4,
-    #          (response.table.y_min + response.table.y_max) / 2,
-    #          0
-    #        ),
-    #        orientation=self._goal_pose.orientation
-    #      )
-    #    )
-    #  )
-    #  # TODO(jstn): doesn't really do anything.
-    #  rospy.loginfo('Driving to table, goal={}'.format(goal))
-    #  self._nav_client.wait_for_server()
-    #  self._nav_client.send_goal(goal)
-    #  self._nav_client.wait_for_result() # Navigation returns nothing.
-    #  rospy.loginfo('Drove to table.')
-    #  state = self._nav_client.get_state()
-    #  if state != GoalStatus.SUCCEEDED:
-    #    rospy.logerr('Failed to precisely navigate to table.')
-    #    return 'nav_to_table_failure'
-
-    #  return 'nav_to_table_success'
-    #except rospy.ServiceException:
-    #  return 'nav_to_table_failure'
-
-class PreparePickObject(smach.State):
-  """Prepares the robot to pick up an object on a table.
-
-  It raises the base and deploys the arms out in front of it.
+class Reset(smach.State):
+  """Resets the robot to its starting position.
   """
 
-  def __init__(self, torso_client, tuck_arms_client):
+  def __init__(self, nav_client, r_gripper_client, tuck_arms_client, goal_pose):
     """Constructor.
 
+    It opens and closes its gripper to release any objects it's holding. Then,
+    it tucks its arms and drives back to the given position.
+
     Args:
-      torso_client: An action client for the PR2 torso.
+      nav_client: An action client for moving the PR2's base.
+      r_gripper_client: An action client for opening the right gripper.
+      tuck_arms_client: An action client for tucking the PR2's arms.
+      goal_pose: Where to go.
     """
     smach.State.__init__(
       self,
-      outcomes=['prepare_pick_success', 'prepare_pick_failure']
+      outcomes=['reset_success', 'reset_failure']
     )
-    self._torso_client = torso_client
+    self._nav_client = nav_client
+    self._r_gripper_client = r_gripper_client
     self._tuck_arms_client = tuck_arms_client
+    self._goal_pose = goal_pose
 
   def execute(self, userdata):
-    rospy.loginfo('Executing PreparePickObject')
-    self._torso_client.wait_for_server()
-    # TODO(jstn): raise to a height based on the table's height.
-    goal = SingleJointPositionGoal(
-      position=0.20,
-      min_duration=rospy.Duration.from_sec(1),
-      max_velocity=1
+    rospy.loginfo('Executing Reset')
+
+    self._r_gripper_client.wait_for_server()
+    open_goal = Pr2GripperCommandGoal(
+      command=Pr2GripperCommand(position=0.08, max_effort=-1)
     )
-    self._torso_client.send_goal(goal)
-    self._torso_client.wait_for_result() # Torso client returns nothing.
+    self._r_gripper_client.send_goal(open_goal)
+    self._r_gripper_client.wait_for_result()
+    if not check_success_state(self._r_gripper_client):
+      rospy.logerr('Failed to open right gripper.')
+      return 'reset_failure'
+
+    close_goal = Pr2GripperCommandGoal(
+      command=Pr2GripperCommand(position=0, max_effort=50)
+    )
+    self._r_gripper_client.send_goal(close_goal)
+    self._r_gripper_client.wait_for_result()
+    if not check_success_state(self._r_gripper_client):
+      rospy.logerr('Failed to close right gripper.')
+      return 'reset_failure'
 
     self._tuck_arms_client.wait_for_server()
-    goal = TuckArmsGoal(tuck_left=False, tuck_right=False)
-    self._tuck_arms_client.send_goal(goal)
+    tuck_arms_goal = TuckArmsGoal(tuck_left=True, tuck_right=True)
+    self._tuck_arms_client.send_goal(tuck_arms_goal)
     self._tuck_arms_client.wait_for_result()
-    result = self._tuck_arms_client.get_result()
-    if result is None:
-      return 'prepare_pick_failure'
-    return 'prepare_pick_success'
+    if not check_success_state(self._tuck_arms_client):
+      rospy.logerr('Failed to tuck arms.')
+      return 'reset_failure'
 
+    self._nav_client.wait_for_server()
+    move_base_goal = MoveBaseGoal(
+      target_pose=PoseStamped(
+        header=Header(frame_id='/map'),
+        pose=self._goal_pose
+      )
+    )
+    self._nav_client.send_goal(move_base_goal)
+    self._nav_client.wait_for_result()
+    if not check_success_state(self._nav_client):
+      rospy.logerr('Failed to roughly navigate to starting position.')
+      return 'reset_failure'
+    return 'reset_success'
